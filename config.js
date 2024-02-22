c457076d93a3b926491e234c66512f3ce58e4a8f
Topher <sears.topher@gmail.com>
2024-02-22 14:15:04 -0500

    udpate node-sass version and site names

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter SolidState', // <title>
+  siteTitle: 'Willow Wellness', // <title>
   manifestName: 'SolidState',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: 'Willow Wellness',
+  subHeading: 'Addressing the roots of your mental health ',
   // social
   socialLinks: [
     {
